919e070877bac98bb3facb9754da7d9e760ea238
Tiago Resende <tiagor87@gmail.com>
2019-03-23 21:55:12 -0300

    Remove DomainException from analysis

--- src/Core/DomainException.cs
+++ src/Core/DomainException.cs
@@ -1,26 +1,12 @@
 using System;
-using System.Diagnostics.CodeAnalysis;
-using System.Runtime.Serialization;
 
 namespace Seedwork.CQRS.Core
 {
-    [Serializable]
+    //NOSONAR
     public abstract class DomainException : Exception
     {
         protected DomainException(string message) : base(message)
         {
-        }
-
-        [ExcludeFromCodeCoverage]
-        private DomainException(SerializationInfo info, StreamingContext context)
-            : base(info, context)
-        {
-        }
-
-        [ExcludeFromCodeCoverage]
-        public override void GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-            base.GetObjectData(info, context);
         }
     }
 }
