34593a22d902a08359f4a0089bb39f6525389053
Tiago Resende <tiagor87@gmail.com>
2019-03-23 21:12:22 -0300

    Implements Serializable pattern

--- src/Core/DomainException.cs
+++ src/Core/DomainException.cs
@@ -1,11 +1,26 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Serialization;
 
 namespace Seedwork.CQRS.Core
 {
+    [Serializable]
     public abstract class DomainException : Exception
     {
         protected DomainException(string message) : base(message)
         {
+        }
+
+        [ExcludeFromCodeCoverage]
+        private DomainException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+
+        [ExcludeFromCodeCoverage]
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
         }
     }
 }
